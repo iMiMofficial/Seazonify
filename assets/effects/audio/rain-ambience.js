11087bcee8056379dbe8688792907a828655aaaf
Md Mim Akhtar <mdmimakhtar@gmail.com>
2025-07-26 13:54:07 +0530

    Update rain-ambience.js
    Signed-off-by: Md Mim Akhtar <mdmimakhtar@gmail.com>

--- assets/effects/audio/rain-ambience.js
+++ assets/effects/audio/rain-ambience.js
@@ -8,7 +8,7 @@ const rainAmbienceEffect = {
   type: "audio",
   author: "Seazonify",
   thumbnail: "https://cdn.jsdelivr.net/gh/iMiMofficial/Seazonify@main/assets/effects/audio/thumbnails/rain-ambience.webp",
-  url: "https://cdn.jsdelivr.net/gh/iMiMofficial/Seazonify@main/assets/effects/audio/mp3/rain-ambience.mp3",
+  url: "https://cdn.jsdelivr.net/gh/iMiMofficial/Seazonify@main/assets/effects/audio/mp3/rain-ambience-1.mp3",
   version: "1.0.0",
   created: "2024-01-15",
   category: "summer",
